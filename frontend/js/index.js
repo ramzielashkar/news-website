ae32acea1571c20aa32e948497239be58fddc206
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-09-23 12:22:52 +0300

    JS: Added News Cards Dynamicly

---
+++ frontend/js/index.js
@@ -0,0 +1,19 @@
+const newsSection = document.querySelector('.news');
+
+
+// function to display news
+const displayNews = () => {
+  const card = `<div class="card" style="width: 18rem;">
+    <img src="images/logo.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+`;
+newsSection.innerHTML+= card;
+};
+
+
+displayNews();
